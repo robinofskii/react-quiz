8560845f66feca0b0b5c4d15c7f34c5ff86bba8a
robinofskii <rvandenberg@yieldcomputer.com>
2023-09-09 21:22:52 +0200

    feat: :sparkles: add error and loading components

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,11 @@
+import './Error.scss';
+
+const Error = () => {
+  return (
+    <div className="error-generic">
+      <p>Oops! Something went wrong. Please try again later.</p>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import './Loader.scss';
+
+const Loader = () => {
+  return (
+    <div className="loader-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
